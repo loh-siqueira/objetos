1eb844b94fa19dcde38f1ca54fd40d087ed57f2d
loh-siqueira <128156957+loh-siqueira@users.noreply.github.com>
2024-06-10 09:05:59 -0300

    Add files via upload

---
+++ aula01/objetos.js
@@ -0,0 +1,8 @@
+const informacoespesoal=['nome','heloa','idade',17,'123.345.567.67'];
+
+const objetospesoa = {
+    nome:"heloa",
+    idade:17,
+    cpf:"123.345.567.67",
+    email:"hela@escola.pr.gov.br",
+}
---
+++ aula02/tipos.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome: "loh",
+    idade: 17,
+    email: "heloa@escola.pr.gov.br",
+    telefone: ["(43)996563719" , "(43) 993426545"],
+};
+console.log(cliente);
